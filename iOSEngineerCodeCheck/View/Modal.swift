d9a03956ce8b0dfa698597540f58530de9c909bd
genki-takami <takamigenki7@gmail.com>
2022-02-17 19:22:08 +0900

    [rename,update]HUDのクラス名をModalに変更、改行の見難さを改善

--- iOSEngineerCodeCheck/View/Modal.swift
+++ iOSEngineerCodeCheck/View/Modal.swift
@@ -9,27 +9,34 @@
 import Foundation
 import SVProgressHUD
 
-final class DisplayPop {
+/// 画面に処理中のモーダルやエラー内容を表示
+final class Modal {
     
     private static var initializing = false
     
-   static func error(_ text: String) {
-        if initializing == false { setting() }
+    static func showError(_ text: String) {
+        if initializing == false {
+            setting()
+        }
         SVProgressHUD.showError(withStatus: text)
     }
     
-    static func show() {
-        if initializing == false { setting() }
+    static func showModal() {
+        if initializing == false {
+            setting()
+        }
         SVProgressHUD.show()
     }
     
-    static func dismiss() {
-        if initializing == false { setting() }
+    static func dismissModal() {
+        if initializing == false {
+            setting()
+        }
         SVProgressHUD.dismiss()
     }
     
+    /// SVProgressHUDのセットアップ
     private static func setting() {
-        // SVProgressHUDのセットアップ
         SVProgressHUD.setDefaultMaskType(.black)
         SVProgressHUD.setMinimumDismissTimeInterval(1)
         initializing = true
