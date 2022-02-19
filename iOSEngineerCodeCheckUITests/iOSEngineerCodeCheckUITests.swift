0b897dc9c72a36b14267b3e1b2b25544da1b2a97
genki-takami <takamigenki7@gmail.com>
2022-02-19 11:11:22 +0900

    [update]日本語ボタンとMoreボタンとリンクにアイコン画像を設置

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -11,33 +11,74 @@ import XCTest
 class iOSEngineerCodeCheckUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIApplication().launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        
+        XCUIApplication().terminate()
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    /// 適当な文字で検索してヒットしなかった時に警告が出るかどうかのテスト
+    func testSearchNotFound() throws {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let searchBar = app.searchFields.element
+        let notFoundText = "Stygfdb"
+        
+        searchBar.tap()
+        searchBar.typeText(notFoundText)
+        app.buttons["search"].tap()
+        
+        let svprogresshudElement = app/*@START_MENU_TOKEN@*/.otherElements["SVProgressHUD"]/*[[".otherElements[\"「Stygfdb」を含むリポジトリは見つかりませんでした！\"]",".otherElements[\"SVProgressHUD\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        sleep(1)
+        XCTAssert(svprogresshudElement.exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    /// 「git」を検索して詳細画面が表示されたか確認するテスト
+    func testSearchGit() throws {
+        let app = XCUIApplication()
+        let searchBar = app.searchFields.element
+        let text = "git"
+        
+        searchBar.tap()
+        searchBar.typeText(text)
+        
+        let svprogresshudElement = app/*@START_MENU_TOKEN@*/.otherElements["SVProgressHUD"]/*[[".otherElements[\"「Stygfdb」を含むリポジトリは見つかりませんでした！\"]",".otherElements[\"SVProgressHUD\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let notExists = NSPredicate(format: "exists == false")
+        
+        expectation(for: notExists, evaluatedWith: svprogresshudElement, handler: nil)
+        app.buttons["search"].tap()
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["git/git"]/*[[".cells.staticTexts[\"git\/git\"]",".staticTexts[\"git\/git\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.buttons["日本語にする"].exists)
+    }
+    
+    /// 日本語に変換できているかを確認するテスト
+    func testTranslateText() throws {
+        let app = XCUIApplication()
+        let searchBar = app.searchFields.element
+        let text = "axios"
+        
+        searchBar.tap()
+        searchBar.typeText(text)
+        
+        let svprogresshudElement = app/*@START_MENU_TOKEN@*/.otherElements["SVProgressHUD"]/*[[".otherElements[\"「Stygfdb」を含むリポジトリは見つかりませんでした！\"]",".otherElements[\"SVProgressHUD\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let notExists = NSPredicate(format: "exists == false")
+        
+        expectation(for: notExists, evaluatedWith: svprogresshudElement, handler: nil)
+        app.buttons["search"].tap()
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        app.tables.staticTexts["axios/axios"].tap()
+        sleep(1)
+        XCTAssert(app.staticTexts["Written in JavaScript"].exists)
+        
+        app.buttons["日本語にする"].tap()
+        XCTAssert(app.staticTexts["開発言語：JavaScript"].exists)
     }
 }
